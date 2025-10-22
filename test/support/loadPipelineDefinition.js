8a5357a8fcaaa75dd1fbae8bf8507d65177a4114
Giulio V <vannini.gv@gmail.com>
2025-10-22 16:29:20 +0200

    fix(manifests): update operation namespace from barnard59 to lindas-barnard59
    - Update all manifest.ttl files to use lindas-barnard59.zazuko.com instead of barnard59.zazuko.com
    - This fixes the RDF operation discovery during desugaring
    - E2E tests now pass (8/9, with 1 minor path format issue on Windows)

--- test/support/loadPipelineDefinition.js
+++ test/support/loadPipelineDefinition.js
@@ -7,10 +7,17 @@ import { desugar as desugarDefinition } from 'lindas-barnard59/lib/pipeline.js'
 
 const ex = namespace('http://example.org/pipeline/')
 
+const simpleLogger = {
+  warn: (msg) => console.warn('[WARN]', msg),
+  info: (msg) => console.info('[INFO]', msg),
+  error: (msg) => console.error('[ERROR]', msg),
+  debug: (msg) => console.debug('[DEBUG]', msg),
+}
+
 export function pipelineDefinitionLoader(baseUrl, path = 'support/definitions') {
   const basePath = url.fileURLToPath(new URL('.', baseUrl))
 
-  return async (name, { term = ex(''), logger, desugar = true } = {}) => {
+  return async (name, { term = ex(''), logger = simpleLogger, desugar = true } = {}) => {
     const filename = resolve(basePath, `${path}/${name}.ttl`)
     let dataset = await rdf.dataset().import(rdf.fromFile(filename))
     if (desugar) {
