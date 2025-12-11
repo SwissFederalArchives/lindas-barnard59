2a4935d457a3e4ede3b899e00a1622588968942c
Giulio Vannini <vannini.gv@gmail.com>
2025-12-11 15:06:19 +0100

    Add type declarations for @lindas/clownface
    The .gitignore was excluding *.d.ts files which prevented the type
    declarations from being committed. Added exception for types/ directory.

---
+++ types/lindas__clownface/index.d.ts
@@ -0,0 +1,6 @@
+// Type declarations for @lindas/clownface
+// Re-export all types from @types/clownface
+declare module '@lindas/clownface' {
+  export * from 'clownface'
+  export { default } from 'clownface'
+}
